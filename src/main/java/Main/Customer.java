c0e11fe2800f02bcfdfc31d46e463c05201609d5
alexmohor1996 <mohor.alexandru@outlook.com>
2024-03-22 15:52:35 +0200

    Added the Customer Details form

--- src/main/java/Main/Customer.java
+++ src/main/java/Main/Customer.java
@@ -1,13 +1,18 @@
 package Main;
 
+import java.util.Scanner;
+import java.util.Vector;
+
 public class Customer {
     private String firstName;
     private String lastName;
     private String dateOfBirth;
     private String billingAddress;
     private String deliveryAddress;
-    private double phoneNumber;
+    private String phoneNumber;
     private String emailAddress;
+
+    Vector<String> userInformation = new Vector<>();
 
     public String getFirstName() {
         return firstName;
@@ -29,7 +34,7 @@ public class Customer {
         return deliveryAddress;
     }
 
-    public double getPhoneNumber() {
+    public String getPhoneNumber() {
         return phoneNumber;
     }
 
@@ -57,12 +62,55 @@ public class Customer {
         this.deliveryAddress = deliveryAddress;
     }
 
-    public void setPhoneNumber(double phoneNumber) {
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
+    }
+
+    public void placeOrder(){
+        System.out.println("Thank you for your order" + getFirstName() + " " + getLastName());
+    }
+
+    public void completeForm(){
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Please enter your first name");
+        String firstName = userInput.nextLine();
+
+        System.out.println("Please enter your last name");
+        String lastName = userInput.nextLine();
+
+        System.out.println("Please enter your date of birth");
+        String dateOfBirth = userInput.nextLine();
+
+        System.out.println("Please enter your billing address");
+        String billingAddress = userInput.nextLine();
+
+        System.out.println("Please enter your delivery address");
+        String deliveryAddress = userInput.nextLine();
+
+        System.out.println("Please enter your phone number");
+        String phoneNumber = userInput.nextLine();
+
+        System.out.println("Please enter your email address");
+        String emailAddress = userInput.nextLine();
+
+        userInformation.add(firstName);
+        userInformation.add(lastName);
+        userInformation.add(dateOfBirth);
+        userInformation.add(billingAddress);
+        userInformation.add(deliveryAddress);
+        userInformation.add(phoneNumber);
+        userInformation.add(emailAddress);
+
+        System.out.println("Thank you for completing your information");
+    }
+
+    public void getUserInformation(){
+        System.out.println("User Information: \n" + userInformation);
     }
 }
 
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -3,6 +3,8 @@ package Main;
 ////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+import java.util.Scanner;
+
 // Create the customer information
 // Create the driver information
 // Create the billing and delivery address form for the customer
@@ -11,6 +13,8 @@ package Main;
 //
 public class Main {
     public static void main(String[] args) {
-
+        Customer newCustomer = new Customer();
+        newCustomer.completeForm();
+        newCustomer.getUserInformation();
     }
 }
