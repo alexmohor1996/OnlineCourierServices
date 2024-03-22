6884b5b379e8604b8c7b8be0d8d6ad813592b549
alexmohor1996 <mohor.alexandru@outlook.com>
2024-03-22 14:43:17 +0200

    initial commit

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+// Create the customer information
+// Create the driver information
+// Create the billing and delivery address form for the customer
+// Create the online ordering form
+// Create the despatch information system
+// 
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
